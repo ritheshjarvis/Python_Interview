02ee6537ef7b39a063d6da87340a39c63302677f
Rithesh Bhaskara <rithesh.bhaskara@honeywell.com>
2025-02-02 12:16:04 +0530

    Binary search explanation

--- Programs/binary_search.py
+++ Programs/binary_search.py
@@ -1,3 +1,5 @@
+# Binary Search
+
 input_list = [3, 6, 8, 9, 10]
 search = 2
 index = -1
@@ -16,3 +18,15 @@ while(start <= end):
         start = mid + 1
         
 print(f'Element found at : {index}' if index != -1 else "Element Not found")
+
+'''
+Binary search is an efficient searching algorithm used to find an element in a sorted array. 
+It follows the divide and conquer approach, repeatedly dividing the search space in half until the element is found or the search space is empty.
+
+Algorithm Steps:
+Find the middle element of the array.
+If the middle element is the target value, return its index.
+If the target is smaller than the middle element, repeat the search on the left half.
+If the target is greater than the middle element, repeat the search on the right half.
+Continue this process until the element is found or the search space is empty.
+'''
