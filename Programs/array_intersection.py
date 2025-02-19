6b455d8da507639fcaa64efef0fac1911e5a91aa
Rithesh Bhaskara <rithesh.bhaskara@honeywell.com>
2025-02-19 09:20:38 +0530

    Selenium Practice

---
+++ Programs/array_intersection.py
@@ -0,0 +1,17 @@
+# Find the intersection of two arrays.
+
+arr1 = [3, 5, 9, 0, 3, 1]
+arr2 = [2, 4, 8, 0, 5, 3]
+output = []
+
+# Method 1 - Logic
+for item in arr1:
+    if item in arr2:
+        output.append(item)
+
+print(output)
+
+# Method 2 - Set Intersection use
+
+output = list(set(arr1).intersection((set(arr2))))
+print(output)
